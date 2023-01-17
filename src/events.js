fda6f11fa18a912e418fb9bbd8dfe0a58dab0d57
Andra <94204279+asteele1493@users.noreply.github.com>
2023-01-16 19:48:34 -0800

    Added lab 11

---
+++ src/events.js
@@ -0,0 +1,7 @@
+const { EventEmitter } = require('events');
+const chance = require('chance')();
+
+
+const events = new EventEmitter();
+
+module.exports = { events, chance };
--- vendor/handler.js
+++
@@ -1,1 +0,0 @@
-//1. Vendor sends pickup event for a store
