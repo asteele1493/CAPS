afcc062f0811e90cc08ad4503d21c0622c829eca
Andra <94204279+asteele1493@users.noreply.github.com>
2023-01-18 18:33:29 -0800

    restructured format

--- src/hub.js
+++ src/hub.js
@@ -1,9 +1,18 @@
-const { startDriver } = require("./driver/handler");
-const { startVendor } = require("./vendor/handler");
+// const { startDriver } = require("./driver/handler");
+// const { startVendor } = require("./vendor/handler");
+const { Server } = require('socket.io');
 
+const io = new Server(3333);
+// startDriver();
+// startVendor();
 
+function startEventServer(){
+  io.on('connection', () => {
+    console.log('Have new connection!');
+  })
+  console.log("Everything is started");
 
-startDriver();
-startVendor();
+}
 
-console.log("Everything is started");
+startEventServer(); 
+
---
+++ src/utils.js
@@ -0,0 +1,9 @@
+const { EventEmitter } = require('events');
+const chance = require('chance')();
+
+
+const events = new EventEmitter();
+
+
+
+module.exports = { chance };
